cfe7744e0c86400a160edeb2dd61ef6f19bf7c5f
codywest <cww2697@gmail.com>
2020-02-10 17:11:14 -0600

    Update with methods

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Hotel {
     public ArrayList<Room> rooms; // Create a list of rooms from the room object.
@@ -6,16 +8,67 @@ public class Hotel {
     public Hotel() {
         rooms = new ArrayList<>();
         for (int i = 100; i < 150; i++) {
-            rooms.add(new Room(i, false));
+            rooms.add(new Room(i, false, true));
         }
         // Add rooms to the hotel
     }
 
-    public static Room changeVacc(int rmNumber) {
+    public static Room changeVacc(Hotel hotel, int rmNumber) {
         // Change vaccancy on selected room
+        Scanner sc = new Scanner(System.in);
+        Room rm = hotel.rooms.get(rmNumber);
+        if (rm.vaccancy){ //mark the room as occupied
+            rm.vaccancy = false;
+            System.out.println("Enter Guest name: ");
+            rm.Guest = sc.nextLine();
+        }
+        else if (!rm.vaccancy){ //mark the room as vaccant
+            rm.vaccancy = true;
+        }
+        return rm;
     }
 
-    public static Room closeRoom(int rmNumber) {
+    public static Room closeRoom(Hotel hotel, int rmNumber) {
         // Mark a room as closed or open
+        Room rm = hotel.rooms.get(rmNumber);
+        if (rm.open){ //close the room
+            rm.open = false;
+        }
+        else if (!rm.open){ //open the room
+            rm.open = true;
+        }
+        return rm;
+    }
+
+    public static void listavai(Hotel hotel){ //list available rooms
+        System.out.print("\t\tAvailable Rooms\t\t\n");
+        System.out.print("-----------------------------------------------\n");
+        for (int i = 100; i < 150; i++) {
+            Room rm = hotel.rooms.get(i);
+            if(rm.vaccancy && rm.open){
+                System.out.print("|\t"+i+"\t"+"|\t"+rm.vaccancy+"\t"+"|\t"+rm.open+"\t"+"|");
+            }
+        }
+    }
+
+    public static void listclosed(Hotel hotel){ //list closed rooms
+        System.out.print("\t\tClosed Rooms\t\t\n");
+        System.out.print("-----------------------------------------\n");
+        for (int i = 100; i < 150; i++) {
+            Room rm = hotel.rooms.get(i);
+            if(!rm.open){
+                System.out.print("|\t"+i+"\t"+"|\t"+rm.open+"\t"+"|");
+            }
+        }
+    }
+    public static void listocc(Hotel hotel){ //List all occupied rooms
+        System.out.print("\t\tOccupied Rooms\t\t\n");
+        System.out.print("-----------------------------------------\n");
+        for (int i = 100; i < 150; i++) {
+            Room rm = hotel.rooms.get(i);
+            if(!rm.vaccancy){
+                System.out.print("|\t"+i+"\t"+"|\t"+rm.Guest+"\t"+"|");
+            }
+        }
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,12 +1,14 @@
 public class Room {
     // this represents the room object and the properties of the room.
-    private int number;
-    private boolean vaccancy;
-    private boolean open;
+    public int number;
+    public boolean vaccancy;
+    public boolean open;
+    public String Guest;
 
     Room(int num, boolean vacc, boolean isOpen) {
         number =  num;
         vaccancy = vacc;
         open = isOpen;
+        Guest = null; //Guest is null by default
     }
 }
