cc9310298de06d8b8f08a4a3ee1265567813146e
Kroren <tak4506@gmail.com>
2020-02-10 10:31:33 -0700

    initial commit, project structure.

---
+++ src/Hotel.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Hotel {
+    public ArrayList<Room> rooms; // Create a list of rooms from the room object.
+
+    public Hotel() {
+        // Add rooms to the hotel
+    }
+}
---
+++ src/Room.java
@@ -0,0 +1,12 @@
+public class Room {
+    // this represents the room object and the properties of the room.
+    private int number;
+    private boolean vaccancy;
+    private char roomType;
+
+    Room(int num, boolean vacc, char type) {
+        number =  num;
+        vaccancy = vacc;
+        roomType = type;
+    }
+}
