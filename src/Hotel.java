d415b74aedd6883fde0ba3a0eb23dd895a059249
codywest <cww2697@gmail.com>
2020-02-11 21:41:59 -0600

    Update with methods

--- src/Hotel.java
+++ src/Hotel.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Hotel {
     public ArrayList<Room> rooms; // Create a list of rooms from the room object.
 
-    public Hotel() {
+    public Hotel() { //Initial constructor
         rooms = new ArrayList<>();
         for (int i = 100; i < 150; i++) {
             rooms.add(new Room(i, false, true));
@@ -13,7 +13,7 @@ public class Hotel {
         // Add rooms to the hotel
     }
 
-    public static Room changeVacc(Hotel hotel, int rmNumber) {
+    public static Room changeVacc(Hotel hotel, int rmNumber) { //employee accessible
         // Change vaccancy on selected room
         Scanner sc = new Scanner(System.in);
         Room rm = hotel.rooms.get(rmNumber);
@@ -29,7 +29,7 @@ public class Hotel {
         return rm;
     }
 
-    public static Room closeRoom(Hotel hotel, int rmNumber) {
+    public static Room closeRoom(Hotel hotel, int rmNumber) { //employee accessible
         // Mark a room as closed or open
         Room rm = hotel.rooms.get(rmNumber);
         if (rm.open){ //close the room
@@ -41,7 +41,7 @@ public class Hotel {
         return rm;
     }
 
-    public static void listavai(Hotel hotel){ //list available rooms, available to  and employees
+    public static void listavai(Hotel hotel){ //list available rooms, available to guest and employees
         System.out.print("\t\tAvailable Rooms\t\t\n");
         System.out.print("-----------------------------------------------\n");
         for (int i = 100; i < 150; i++) {
@@ -52,7 +52,7 @@ public class Hotel {
         }
     }
 
-    public static void listclosed(Hotel hotel){ //list closed rooms
+    public static void listclosed(Hotel hotel){ //list closed rooms - employee accessible
         System.out.print("\t\tClosed Rooms\t\t\n");
         System.out.print("-----------------------------------------\n");
         for (int i = 100; i < 150; i++) {
@@ -62,7 +62,7 @@ public class Hotel {
             }
         }
     }
-    public static void listocc(Hotel hotel){ //List all occupied rooms
+    public static void listocc(Hotel hotel){ //List all occupied rooms - employee accessible
         System.out.print("\t\tOccupied Rooms\t\t\n");
         System.out.print("-----------------------------------------\n");
         for (int i = 100; i < 150; i++) {
