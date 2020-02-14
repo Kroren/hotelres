e962b4bbdc8b3edf29cbfe86417e617377fdbcac
Kroren <tak4506@gmail.com>
2020-02-14 15:24:26 -0700

    employeelogin began.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+    Hotel hotel = new Hotel();
     // Main class
     public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
         // Main menu, logins, etc
     }
 
-    public static boolean employeeLogin(String password) {
-        // Login as employee (manage rooms)
-        if (password == "password") {
-            // asdfljasd
-            return true;
-        }
-        else {
-            return false;
+    public static boolean employeeLogin(String password, Scanner scan) {
+        String currentPass = "password";
+        System.out.println("Welcome employee,");
+        System.out.print("Please enter your login password: ");
+        String input = scan.next();
+
+        while (input == currentPass) {
+            employeeMenu(scan);
         }
     }
 
@@ -19,8 +24,34 @@ public class Main {
         // Login as user
     }
 
-    public static void menu() {
-        System.out.println("Main menu");
-        // Code
+    public static void employeeMenu(Scanner s) {
+        System.out.println("\n1. Check rooms available");
+        System.out.println("2. Check occupied rooms");
+        System.out.println("3. Check occupied rooms");
+        System.out.println("4. Check closed rooms");
+        System.out.println("5. Edit rooms");
+        System.out.println("6. Logout");
+
+        try {
+            int choice = s.nextInt();
+            // run methods based on the choice.
+            switch(choice) {
+                case 1:
+
+                case 2:
+
+                case 3:
+
+                case 4:
+
+                case 5:
+
+                case 6:
+                    break;
+            }
+        }
+        catch(IOException) {
+
+        }
     }
 }
