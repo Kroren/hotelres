2d3dc3c11a0fa205fbf052c7a3f20140e9378556
codywest <cww2697@gmail.com>
2020-02-14 20:40:30 -0600

    Update Main to allow for string comparision Update Hotel to print the room number rather than the index

--- src/Hotel.java
+++ src/Hotel.java
@@ -6,9 +6,16 @@ public class Hotel {
     public ArrayList<Room> rooms; // Create a list of rooms from the room object.
 
     public Hotel() { //Initial constructor
-        rooms = new ArrayList<>();
-        for (int i = 100; i < 150; i++) {
-            rooms.add(new Room(i, false, true));
+        try {
+            rooms = new ArrayList<>();
+            for (int i = 100; i < 150; i++) {
+
+                rooms.add(new Room(i, true, true));
+                System.out.println(i+", ");
+            }
+        }
+        catch(Exception IOException){
+            System.out.println("There was an error creating the hotel");
         }
         // Add rooms to the hotel
     }
@@ -44,10 +51,11 @@ public class Hotel {
     public static void listavai(Hotel hotel){ //list available rooms - Guest and Employee
         System.out.print("\t\tAvailable Rooms\t\t\n");
         System.out.print("-----------------------------------------------\n");
-        for (int i = 100; i < 150; i++) {
+        for (int i = 0; i < 50; i++) {
+            int j = i+100;
             Room rm = hotel.rooms.get(i);
             if(rm.vaccancy && rm.open){
-                System.out.print("|\t"+i+"\t"+"|\t"+rm.vaccancy+"\t"+"|\t"+rm.open+"\t"+"|");
+                System.out.print("|\t"+j+"\t"+"|\t"+rm.vaccancy+"\t"+"|\t"+rm.open+"\t"+"|\n");
             }
         }
     }
@@ -55,20 +63,22 @@ public class Hotel {
     public static void listclosed(Hotel hotel){ //list closed rooms - employee accessible
         System.out.print("\t\tClosed Rooms\t\t\n");
         System.out.print("-----------------------------------------\n");
-        for (int i = 100; i < 150; i++) {
+        for (int i = 0; i < 50; i++) {
+            int j = i+100;
             Room rm = hotel.rooms.get(i);
             if(!rm.open){
-                System.out.print("|\t"+i+"\t"+"|\t"+rm.open+"\t"+"|");
+                System.out.print("|\t"+j+"\t"+"|\t"+rm.open+"\t"+"|\n");
             }
         }
     }
     public static void listocc(Hotel hotel){ //List all occupied rooms - employee accessible
         System.out.print("\t\tOccupied Rooms\t\t\n");
         System.out.print("-----------------------------------------\n");
-        for (int i = 100; i < 150; i++) {
+        for (int i = 0; i < 50; i++) {
+            int j = i+100;
             Room rm = hotel.rooms.get(i);
             if(!rm.vaccancy){
-                System.out.print("|\t"+i+"\t"+"|\t"+rm.Guest+"\t"+"|");
+                System.out.print("|\t"+j+"\t"+"|\t"+rm.Guest+"\t"+"|\n");
             }
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,25 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    static Hotel hotel = new Hotel();
     // Main class
     public static void main(String args[]) {
+        Hotel hotel = new Hotel();
         Scanner input = new Scanner(System.in);
         // Main menu, logins, etc
         boolean repeatMenu = true;
-        while(repeatMenu){
+        while (repeatMenu) {
             System.out.println("\nMain menu");
             System.out.println("1) Guest Login");
             System.out.println("2) Employee Login");
             System.out.println("3) Exit");
             try {
                 int menuChoice = input.nextInt();
-                switch(menuChoice) {
+                switch (menuChoice) {
                     case 1:
-                        guestLogin(input);
+                        guestLogin(input, hotel);
                         break;
                     case 2:
-                        employeeLogin(input);
+                        employeeLogin(input, hotel);
                         break;
                     case 3:
                         repeatMenu = false;
@@ -28,60 +28,60 @@ public class Main {
                     default:
                         System.out.println("Invalid Choice.");
                 }
-            }
-            catch(Exception IOException){
-
+            } catch (Exception IOException) {
+                System.out.println("There was an error");
             }
         }
     }
 
-    public static void employeeLogin(Scanner input) {
+    public static void employeeLogin(Scanner input, Hotel hotel) {
         String password = "password";
         System.out.println("Welcome employee,");
         System.out.print("Please enter your login password: ");
         String passwordCheck = input.next();
 
-        if (passwordCheck == password) {
-            employeeMenu(input);
+        if (passwordCheck.equals(password)) {
+            employeeMenu(input, hotel);
         }
     }
 
-    public static void guestLogin(Scanner input) {
+    public static void guestLogin(Scanner input, Hotel hotel) {
         String password = "password";
         System.out.println("Welcome guest,");
         System.out.print("Please enter your login password: ");
         String passwordCheck = input.next();
 
-        if (passwordCheck == password) {
-            guestMenu(input);
+        if (passwordCheck.equals(password)) {
+            guestMenu(input, hotel);
         }
     }
 
-    public static void guestMenu(Scanner input) {
+    public static void guestMenu(Scanner input, Hotel hotel) {
         // Login as user
         System.out.println("\n1) Check rooms available");
         System.out.println("2) Logout");
         boolean repeatMenu = true;
-        while(repeatMenu){
-        try {
-            int choice = input.nextInt();
-            // run methods based on the choice.
-            switch(choice) {
-                case 1:
-                    Hotel.listavai(hotel);
-                    break;
-                case 2:
-                    repeatMenu = false;
-                default:
-                    System.out.println("Invalid Choice.");
+        while (repeatMenu) {
+            try {
+                int choice = input.nextInt();
+                // run methods based on the choice.
+                switch (choice) {
+                    case 1:
+                        Hotel.listavai(hotel);
+                        break;
+                    case 2:
+                        repeatMenu = false;
+                    default:
+                        System.out.println("Invalid Choice.");
+                }
+            } catch (Exception IOException) {
+                System.out.println("There was an error");
             }
-        }
-        catch(Exception IOException) {
-        }
         }
     }
 
-    public static void employeeMenu(Scanner input) {
+
+    public static void employeeMenu(Scanner input, Hotel hotel) {
         System.out.println("\n1) Check rooms available");
         System.out.println("2) Check occupied rooms");
         System.out.println("3) Check closed rooms");
@@ -130,6 +130,7 @@ public class Main {
             }
         }
         catch(Exception IOException) {
+            System.out.println("There was an error");
         }
         }
     }
