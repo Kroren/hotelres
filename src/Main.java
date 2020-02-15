4eb7fa8ae78cf9ed4a3e1577215d3cf5fc7e9b94
thetomzest <ctbednarz7@gmail.com>
2020-02-14 19:27:20 -0600

    Added employee and guest menu systems

--- src/Main.java
+++ src/Main.java
@@ -2,56 +2,132 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    Hotel hotel = new Hotel();
+    static Hotel hotel = new Hotel();
     // Main class
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         // Main menu, logins, etc
-    }
+        boolean repeatMenu = true;
+        while(repeatMenu){
+            System.out.println("\nMain menu");
+            System.out.println("1) Guest Login");
+            System.out.println("2) Employee Login");
+            System.out.println("3) Exit");
+            try {
+                int menuChoice = input.nextInt();
+                switch(menuChoice) {
+                    case 1:
+                        guestLogin(input);
+                        break;
+                    case 2:
+                        employeeLogin(input);
+                        break;
+                    case 3:
+                        repeatMenu = false;
+                        break;
+                    default:
+                        System.out.println("Invalid Choice.");
+                }
+            }
+            catch(Exception IOException){
 
-    public static boolean employeeLogin(String password, Scanner scan) {
-        String currentPass = "password";
-        System.out.println("Welcome employee,");
-        System.out.print("Please enter your login password: ");
-        String input = scan.next();
-
-        while (input == currentPass) {
-            employeeMenu(scan);
+            }
         }
     }
 
-    public static void userLogin() {
-        // Login as user
+    public static void employeeLogin(Scanner input) {
+        String password = "password";
+        System.out.println("Welcome employee,");
+        System.out.print("Please enter your login password: ");
+        String passwordCheck = input.next();
+
+        if (passwordCheck == password) {
+            employeeMenu(input);
+        }
     }
 
-    public static void employeeMenu(Scanner s) {
-        System.out.println("\n1. Check rooms available");
-        System.out.println("2. Check occupied rooms");
-        System.out.println("3. Check occupied rooms");
-        System.out.println("4. Check closed rooms");
-        System.out.println("5. Edit rooms");
-        System.out.println("6. Logout");
+    public static void guestLogin(Scanner input) {
+        String password = "password";
+        System.out.println("Welcome guest,");
+        System.out.print("Please enter your login password: ");
+        String passwordCheck = input.next();
+
+        if (passwordCheck == password) {
+            guestMenu(input);
+        }
+    }
+
+    public static void guestMenu(Scanner input) {
+        // Login as user
+        System.out.println("\n1) Check rooms available");
+        System.out.println("2) Logout");
 
         try {
-            int choice = s.nextInt();
+            int choice = input.nextInt();
             // run methods based on the choice.
             switch(choice) {
                 case 1:
-
-                case 2:
-
-                case 3:
-
-                case 4:
-
-                case 5:
-
-                case 6:
+                    Hotel.listavai(hotel);
                     break;
+                default:
+                    System.out.println("Invalid Choice.");
             }
         }
-        catch(IOException) {
+        catch(Exception IOException) {
 
+        }
+    }
+
+    public static void employeeMenu(Scanner input) {
+        System.out.println("\n1) Check rooms available");
+        System.out.println("2) Check occupied rooms");
+        System.out.println("3) Check closed rooms");
+        System.out.println("4) Edit rooms");
+        System.out.println("5) Logout");
+
+        boolean repeatMenu = true;
+        while(repeatMenu){
+        try {
+            int choice = input.nextInt();
+            // run methods based on the choice.
+            switch(choice) {
+                case 1:
+                    Hotel.listavai(hotel);
+                    break;
+                case 2:
+                    Hotel.listocc(hotel);
+                    break;
+                case 3:
+                    Hotel.listclosed(hotel);
+                    break;
+                case 4:
+                    System.out.println("Enter the room you would like to edit:");
+                    int roomNumber = input.nextInt();
+                    System.out.println("What would you like to do?");
+                    System.out.println("1) Change vaccancy");
+                    System.out.println("2) Close room");
+                    System.out.println("3) Exit");
+                    int menuChoice = input.nextInt();
+                    switch(menuChoice) {
+                        case 1:
+                            Hotel.changeVacc(hotel, roomNumber);
+                            break;
+                        case 2:
+                            Hotel.closeRoom(hotel, roomNumber);
+                            break;
+                        default:
+                            System.out.println("Invalid Choice.");
+                    }
+                    break;
+                case 5:
+                    repeatMenu = false;
+                    break;
+                default:
+                    System.out.println("Invalid Choice.");
+            }
+        }
+        catch(Exception IOException) {
+        }
         }
     }
 }
